d65c6ec22b8cae41045be2116067e5bbb2d0e806
nazkord <nazkord@gmail.com>
2021-01-17 12:24:28 +0100

    split main.dart into smaller classes

---
+++ lib/src/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:product_check/src/views/ui/home.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'NFC Scan'),
+    );
+  }
+}
